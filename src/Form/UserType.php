7f5e77d9185ec11ab4e92b53afe04c2dd78fc657
jcaillieret <jean-baptiste.caillieret@businessdecision.com>
2024-09-26 15:13:12 +0200

    Init crud User

---
+++ src/Form/UserType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Borrowing;
+use App\Entity\Reservation;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('uuid')
+            ->add('roles')
+            ->add('password')
+            ->add('name')
+            ->add('firstName')
+            ->add('email')
+            ->add('penality')
+            ->add('borrowing', EntityType::class, [
+                'class' => Borrowing::class,
+                'choice_label' => 'id',
+            ])
+            ->add('reservation', EntityType::class, [
+                'class' => Reservation::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
