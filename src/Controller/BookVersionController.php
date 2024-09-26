f950e8935028a4a74933e04382a029cf65575a11
jcaillieret <jean-baptiste.caillieret@businessdecision.com>
2024-09-26 16:17:45 +0200

    Init Login/Register

--- src/Controller/BookVersionController.php
+++ src/Controller/BookVersionController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/book/version')]
+#[Route('/book_version')]
 final class BookVersionController extends AbstractController
 {
     #[Route(name: 'app_book_version_index', methods: ['GET'])]
