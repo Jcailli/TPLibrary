0e4f5e7b20d49adf371b73745a2219b8f5e442e6
jcaillieret <jean-baptiste.caillieret@businessdecision.com>
2024-09-27 14:18:56 +0200

    Update User CRUD for administration

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -11,12 +11,27 @@ class UserFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $user = new User();
-        $user->setUuid('admin');
-        $user->setFirstName('admin');
-        $user->setName('admin');
-        $user->setPassword('admin');
+        $user->setUserFirstName('admin');
+        $user->setUsername('admin');
+        $user->setPassword('$2y$13$mByMPKoQy1lXA6Nat9085Oo/iLQ0E6P6bK78C5MVY1yaay4pbxUc.'); //admin
         $user->setEmail('admin@admin.com');
-        $user->setRoles(['ROLE_ADMIN']);
+        $user->setRoles(["ROLE_ADMIN"]);
+        $manager->persist($user);
+
+        $user = new User();
+        $user->setUserFirstName('librarian');
+        $user->setUsername('librarian');
+        $user->setPassword('$2y$13$Xlr1NXEHZWj.Qt50vEcJ1usRJ8Iz7tmFPkPgJ4I6En22w17utSdEi'); //librarian
+        $user->setEmail('librarian@librarian.com');
+        $user->setRoles(["ROLE_LIBRARIAN"]);
+        $manager->persist($user);
+
+        $user = new User();
+        $user->setUserFirstName('user');
+        $user->setUsername('user');
+        $user->setPassword('$2y$13$CsW8X1FU0/L2dg/i90FnOumR9NV9uy806G3Ozv9IVPJUhZqr6sl62'); //user
+        $user->setEmail('user@user.com');
+        $user->setRoles(["ROLE_USER"]);
         $manager->persist($user);
 
         $manager->flush();
