7054bb607e6f703381f5eb86f992e1ab2347ff67
jcaillieret <jean-baptiste.caillieret@businessdecision.com>
2024-09-27 14:15:52 +0200

    Add AppController for the home page

---
+++ src/Controller/AppController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class AppController extends AbstractController
+{
+    public function __construct(
+        private readonly Security $security
+    ){
+
+    }
+
+    #[Route('/', name: 'homepage')]
+    public function index(): Response
+    {
+        $user = $this->security->getUser();
+
+        return $this->render('app/index.html.twig', [
+            'user' => $user,
+        ]);
+    }
+}
